865f5abf026a36329ac8c43de6f930ea1e332ae8
lflambert <53384061+lflambert@users.noreply.github.com>
2020-05-23 09:57:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
 import pandas as pd
+import os from pml
+import app port = int(os.environ.get('PORT', 5000)) 
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
@@ -12,4 +15,4 @@ def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(host='0.0.0.0', port=port)
