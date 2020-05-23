fffc7aeeb7c2576f683055841a4968e5c65b1c32
lflambert <53384061+lflambert@users.noreply.github.com>
2020-05-23 16:45:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,5 @@ def index():
 def about():
   return render_template('about.html')
 
-if __name__ == '__main__':
-port = int(os.environ.get("PORT", 5000))    
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':    
+    app.run(port=5000, debug=true)
